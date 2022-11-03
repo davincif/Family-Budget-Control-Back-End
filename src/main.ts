79c6ad8ccceab395e8fe1eca7821a4cda9fe9ced
rpdavincif <dev26@redepos.com.br>
2022-11-03 17:04:31 -0300

    hello world

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,28 @@
 import dotenv from "dotenv";
 dotenv.config();
 import express from "express";
+import cors, { CorsOptions } from "cors";
 
+// manual initializaitons
 const app = express();
-console.log("app", app);
+
+app.get("/", function (req, res) {
+  const answer = "Lock'n'load, sir! 🤖";
+  res.status(200).send(answer);
+  console.info(answer);
+});
+
+const server = app.listen(process.env.PORT);
+initializeManualMiddlewares({
+  origin: process.env.CORS_ORIGINS?.split(",").map((origin) => origin.trim()),
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+});
+
+server.on("listening", () => {
+  console.info(`🪖 Captan! Waiting orders on port ${process.env.PORT} 🫡`);
+});
+
+function initializeManualMiddlewares(corsOptions: CorsOptions) {
+  console.log("corsOptions", corsOptions);
+  app.use(cors(corsOptions));
+}
